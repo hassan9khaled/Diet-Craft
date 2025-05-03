d145212b7fa26239c8cc1570fd053be9d90438cd
Ziad Tamer <ziadtamer0543@gmail.com>
2025-05-04 00:38:00 +0300

    Test Front

---
+++ FrontEnd/Spinner.jsx
@@ -0,0 +1,9 @@
+function Spinner() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="spinner"></div>
+    </div>
+  );
+}
+
+export default Spinner;
